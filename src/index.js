29967ddee2403706dc128086fd3b312d8ded576b
Johnny Pacia <johnny.pacia@gmail.com>
2020-02-11 21:46:18 -0500

    adds example menu

--- src/index.js
+++ src/index.js
@@ -1,219 +1,41 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Container, Image, Button, Menu, Segment } from 'semantic-ui-react';
-
-export default class MenuExampleSecondary extends Component {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { activeItem } = this.state
-
-    return (
-      <div>
-        <Menu secondary>
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='earn'
-            active={activeItem === 'earn'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='redeem'
-            active={activeItem === 'redeem'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Menu position='right'>
-            <Menu.Item
-              name='logout'
-              active={activeItem === 'logout'}
-              onClick={this.handleItemClick}
-            />
-          </Menu.Menu>
-        </Menu>
-
-        <Segment>
-          
-        </Segment>
-      </div>
-    )
-  }
-}
-
-const ContainerExampleContainer = () => (
-  <Container>
-    <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-      ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-      ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-      quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-      arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-      Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-      dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-      viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-    </p>
-  </Container>
-)
-
-const ImageExampleWrapped = () => (
-  <Image src='/images/wireframe/image.png' size='small' wrapped />
-)
-
-const ButtonExampleButton = () => <Button>Bid Now</Button>
+import { Container, Header, List } from 'semantic-ui-react';
+import Example from "./example";
 
 
-
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return ( 
-      <Square 
-      value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+const App = ({ children }) => (
+  <Container style={{ margin: 20 }}>
+    <Header as="h3">This example is powered by Semantic UI React</Header>
+    <List bulleted>
+      <List.Item
+        as="a"
+        content="Official documentation"
+        href="https://react.semantic-ui.com/"
+        target="_blank"
       />
-     );
-  }
+      <List.Item
+        as="a"
+        content="StackOverflow"
+        href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
+        target="_blank"
+      />
+    </List>
 
-  render() {
+    {children}
+  </Container>
+);
 
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+// TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
-class Game extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i){
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length -1];
-    const squares = current.squares.slice();
-      if (calculateWinner(squares) || squares[i]){
-        return;
-      }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-          squares ={current.squares}
-          onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-// ========================================
-
-// ReactDOM.render(
-//   <Game />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <App>
+    <Example />
+  </App>,
+  document.getElementById("root")
+);
