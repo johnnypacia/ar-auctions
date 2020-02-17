73c1afa9397a4c31c475666461fc7747d4edf425
Johnny Pacia <johnny.pacia@gmail.com>
2020-02-17 16:46:16 -0500

    removes previous change

--- src/auctioncontainer.js
+++ src/auctioncontainer.js
@@ -7,7 +7,7 @@ const AuctionContainer= () => (
       <Image src={'ROA_767x575.jpg'} size='medium' />
       <Header as='h2'>Meet Meghan Picerno and see her The Phantom of the Opera on Broadway!
       </Header>
-      <Header as='h4'>New York, NY  | February 29, 2020 @ 8:00PM EST
+      <Header as='h4'>New York, NY | February 29, 2020 @ 8:00PM EST
       </Header>
       <p>Join us for an exclusive meet & greet following a performance of Andrew Lloyd Webber’s The Phantom of the Opera! After the show ends, you and a guest will be invited to meet the current Christine Daaé and learn more about Broadway’s breathtaking original production. Don’t forget your camera!
       </p>
