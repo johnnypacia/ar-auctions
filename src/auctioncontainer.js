8a28674a3bc77d8f642abed34bad730679085a8c
Johnny Pacia <johnny.pacia@gmail.com>
2020-02-29 18:56:19 -0500

    adds segment menu

--- src/auctioncontainer.js
+++ src/auctioncontainer.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { Container, Segment, Item, Divider } from 'semantic-ui-react'
+import { Container, Segment, Menu, Item, Divider } from 'semantic-ui-react'
 
 const AuctionContainer= () => (
   <div>
     <Container fluid style={{ marginTop: '7em' }}>
       <Segment>
+         <Menu size='mini'>
+            <Menu.Item
+              name='home'
+              active={activeItem === 'home'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='messages'
+              active={activeItem === 'messages'}
+              onClick={this.handleItemClick}
+            />
+
+            <Menu.Menu position='right'>
+              <Dropdown item text='Language'>
+                <Dropdown.Menu>
+                  <Dropdown.Item>English</Dropdown.Item>
+                  <Dropdown.Item>Russian</Dropdown.Item>
+                  <Dropdown.Item>Spanish</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Menu.Item>
+                <Button primary>Sign Up</Button>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
         <Item.Group>
           <Item>
             <Item.Image size='medium' src='bowlroom.jpg' />
