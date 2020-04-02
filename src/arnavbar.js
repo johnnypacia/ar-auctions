930c5cdc16af542d7e1fababb8db211daae24363
Johnny Pacia <johnny.pacia@gmail.com>
2020-04-02 15:10:21 -0400

    adds new navbar

--- src/arnavbar.js
+++ src/arnavbar.js
@@ -14,7 +14,40 @@ export default class MenuExampleSecondary extends Component {
 
     return (
       <React.Fragment>
-       
+        <Menu fixed='top' secondary style={{ marginBottom: '7em', backgroundColor: '#781d4f'}}> 
+          <Menu.Item as='a' header>
+            <Image size='small' src='Logo_158x57.png' style={{ marginRight: '1.5em' }} />
+          </Menu.Item>
+          
+          <Menu.Item style={{ color: '#FFFFFF', backgroundColor: '#521d41', fontFamily: 'Montserrat, sans-serif', textDecoration: 'underline' }}
+            name="earn"
+            active={activeItem === "earn"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item style={{ color: '#FFFFFF', backgroundColor: '#521d41', fontFamily: 'Montserrat, sans-serif', textDecoration: 'underline' }}
+            name="redeem"
+            active={activeItem === "redeem"}
+            onClick={this.handleItemClick}
+          />
+
+          
+          <Menu.Menu position="right">
+            <Menu.Item style={{ color: '#FFFFFF', fontFamily: 'Montserrat, sans-serif', textDecoration: 'none' }}
+              name="sign in"
+              active={activeItem === "sign in"}
+              onClick={this.handleItemClick}
+            />
+
+            <Menu.Item style={{backgroundColor: '#FFFFFF', borderRadius: '50%', color: '#000000' , padding: '5px' , font: 'normal 0.71em Georgia', textDecoration: 'none', fontStyle: 'italic' }} 
+              name="or"
+            />
+            <Menu.Item style={{ color: '#FFFFFF', fontFamily: 'Montserrat, sans-serif', textDecoration: 'none' }}
+              name="join for free"
+              active={activeItem === "join for free"}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
         
         <Menu size='mini' fixed='top' secondary  style={{ marginTop: '6em' , maxWidth: '200em' , backgroundColor: '#781d4f' }}>
 
